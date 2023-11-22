9948beb128ee3aa12cb113034004d68488b167f2
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2023-11-22 11:28:16 +0100

    Add files via upload

---
+++ Ukol1/RetezUtil/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection.Emit;
+using System.Text;
+
+namespace RetezUtil
+{
+    public class Class1
+    {
+        public static void Smaz(ref string veta)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(veta);
+
+            for(int i = 0; i < stringBuilder.Length;) {
+                if (stringBuilder[i] >= '0' && stringBuilder[i] <= '9')
+                {
+                    stringBuilder.Remove(i, 1);
+                }
+                else
+                {
+                    i++;
+                }
+            }   
+            veta = stringBuilder.ToString();
+        }
+
+        public static string[] Zobraz(ref string veta,Label label)
+        {
+            string[] polozky = veta.Split(' ');
+            return polozky;
+        }
+    }
+}
